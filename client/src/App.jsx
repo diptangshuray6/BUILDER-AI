ef97356e4584c08535fce256ae0d265901f00f41
Diptangshu Ray <diptangshuray617@gmail.com>
2026-08-27 14:12:49 +0530

    Code commit upto 2.43.41

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,14 @@ import AuthPage from './pages/AuthPage'
 import HomePage from './pages/Homapage'
 import BuilderPage from './pages/BuilderPage'
 import PreviewPage from './pages/PreviewPage'
+import { Toaster } from 'react-hot-toast'
+
 
 
 const App = () => {
   return (
+    <>
+    <Toaster />
     <Routes>
       {/* Login Routes */}
       <Route element={<GuestLayout />}>
@@ -26,6 +30,8 @@ const App = () => {
       <Route path="*" element={<Navigate to="/" replace />} />
 
     </Routes>
+    </>
+    
   )
 }
 

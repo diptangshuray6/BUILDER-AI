a7aacd409a8e5df7b32c02f1b9a6ea79ae2e4e9a
Diptangshu Ray <diptangshuray617@gmail.com>
2026-09-01 00:08:26 +0530

    upto 6.00.43 changes

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectToDatabase } from "./config/db.js";
+import authRouter from "./Routes/authRoutes.js";
+
+
+const app = express();
+
+await connectToDatabase();
+
+app.use(cors({origin: process.env.ORIGINS.split(","), credentials: true}))
+app.use(cookieParser())
+app.use(express.json())
+
+app.get("/", (req,res) => res.send("Server is Live !!!"));
+app.use('/api/auth', authRouter)
+
+app.use((err,_req,res,_next) => {
+    console.error(`[Error] ${err.message}`);
+    res.status(500).json({error: err.message})
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
+
+
